47b98fe71285aff87b5ee75cd0e9af074f646533
Videonik <155845834+Videonik@users.noreply.github.com>
2024-12-14 10:57:04 +0500

    models.py

--- your_project/models.py
+++ your_project/models.py
@@ -1,4 +1,3 @@
-# models.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
@@ -8,7 +7,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    todos = db.relationship('Todo', backref='owner', lazy=True)  # Связь с задачами
+    todos = db.relationship('Todo', backref='owner', lazy=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
